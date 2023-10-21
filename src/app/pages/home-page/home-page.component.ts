5b6172c915494682ba01f5b1c2b91ab8f9c86a37
Wiky Lyu <wikylyu@outlook.com>
2023-10-21 11:58:20 +0800

    hero talks

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
-
+  talks: any[] = [
+    {
+      text: 'I feel relieved if the United States is under the protection of ClickMan.',
+      name: 'Steven Rogers',
+    },
+    {
+      text: 'ClickMan is the most powerful person I have ever met in my life',
+      name: 'Tony Stark',
+    },
+    {
+      text: 'ClickMan is the only one who can stop Google from collecting the Infinity Stones.',
+      name: 'Stephen Strange',
+    },
+  ];
 }
